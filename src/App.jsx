a88d8f378d740a17abdca35e853f4922b7a963c2
Juan Ignacio Oliva <olicodea@gmail.com>
2023-01-22 17:55:55 -0300

    test de cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>COMPRITAS</h1>
+    </div>
+  )
 }
 
-export default App;
+export default App
